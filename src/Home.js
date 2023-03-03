2a6fd8ab342a0f979eeca5dce11e59f407844acf
Alex <alexhallinthecloud@icloud.com>
2023-03-03 12:19:20 -0800

    added chatpage route

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div>
+            <h1>CATtention</h1>
+            <p>Home Page</p>
+        </div>
+    );
+}
+
+export default HomePage;
