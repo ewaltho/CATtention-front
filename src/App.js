734abbba175063dfdbf6272b65686fb3024e8a97
Ben L <veloswim@gmail.com>
2023-03-06 11:02:53 -0800

    added signup component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatFeature from "./components/Chat";
 import HomePage from "./components/Home";
 import JoinChat from "./components/JoinChat";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/chat" element={<JoinChat />} />
         <Route path="/chat/:roomId" element={<ChatFeature />} />
         <Route path="*" element={<h1>404 page not found'</h1>} />
