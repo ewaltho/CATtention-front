31fe9d4a063839ab21139bfe0029d9e6582afafb
Ben L <veloswim@gmail.com>
2023-03-06 11:17:34 -0800

    fixed an issue with axios in API and created form to sign up a user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import "./assets/css/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import API from "./utils/API";
 import ChatFeature from "./components/Chat";
 import HomePage from "./components/Home";
 import JoinChat from "./components/JoinChat";
 import SignUp from "./components/SignUp";
 
 function App() {
+  const [signUpFormData, setSignUpFormData] = useState({});
+
+  const handleSignUpFormChange = (e) => {
+    e.preventDefault();
+    setSignUpFormData({
+      ...signUpFormData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignUpFormSubmit = async (e) => {
+    e.preventDefault();
+    const response = await API.signUpUser(signUpFormData);
+    console.log(response);
+    return response;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/signup"
+          element={
+            <SignUp
+              handleSignUpFormChange={handleSignUpFormChange}
+              handleSignUpFormSubmit={handleSignUpFormSubmit}
+            />
+          }
+        />
         <Route path="/chat" element={<JoinChat />} />
         <Route path="/chat/:roomId" element={<ChatFeature />} />
         <Route path="*" element={<h1>404 page not found'</h1>} />
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function SignUp() {
-  return <div>SignUp</div>;
+export default function SignUp({
+  handleSignUpFormChange,
+  handleSignUpFormSubmit,
+}) {
+  return (
+    <form onSubmit={handleSignUpFormSubmit}>
+      <label htmlFor="username">Username:</label>
+      <input
+        onChange={handleSignUpFormChange}
+        name="username"
+        placeholder="username"
+      ></input>
+      <label htmlFor="password">Password:</label>
+      <input
+        onChange={handleSignUpFormChange}
+        name="password"
+        placeholder="password"
+        type="password"
+      ></input>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
