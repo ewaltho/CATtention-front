bb61ce597872df0502c282b42c177e5b063eda7f
VisualViolet <savannah_b_miller@outlook.com>
2023-03-06 14:46:04 -0800

    placeholder css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ export default function Navigation() {
     return (
         <div className='Nav'>
             <img src='CATtention-logo.png' alt='logo' className='logo'/>
-            <Link to="/login">Login</Link>
+            <Link to="/login" className='login-btn'>Login</Link>
         </div>
     )
 }
