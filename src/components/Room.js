15783d41c85f2ff8988335bb7dc631d334126bd9
Ben L <veloswim@gmail.com>
2023-03-07 14:10:35 -0800

    added Room.js

---
+++ src/components/Room.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Chat from "./Chat";
+
+export default function Room(props) {
+  return (
+    <>
+      <div className="room">
+        <div>
+          <p>Placeholder text for timer/minigame</p>
+        </div>
+        <Chat roomData={props.roomData} />
+      </div>
+    </>
+  );
+}
