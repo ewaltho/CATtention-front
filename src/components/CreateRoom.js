c6ba6f5b168f3ca57cbe18ee3f002c516a9c05cb
Ben L <veloswim@gmail.com>
2023-03-08 11:12:17 -0800

    updates

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function CreateRoom() {
+export default function CreateRoom(props) {
+  const [roomPreferences, setRoomPreferences] = useState({});
+
   return <div>CreateRoom</div>;
 }
