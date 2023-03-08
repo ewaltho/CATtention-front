a244f52868758f44ad2b2658ff53053f6a5b9135
Ben L <veloswim@gmail.com>
2023-03-07 17:32:36 -0800

    added cats and update user api endpoints

--- src/utils/API.js
+++ src/utils/API.js
@@ -49,6 +49,25 @@ const API = {
     console.log(response);
     return response;
   },
+  updateUser: async (userObj, userId) => {
+    const response = await axios.put(
+      `${URL_PREFIX}/api/users/${userId}`,
+      userObj
+    );
+
+    console.log(response);
+    return response;
+  },
+  allCats: async () => {
+    const allCats = await axios.get(`${URL_PREFIX}/api/cats`);
+
+    return allCats;
+  },
+  oneCat: async (catId) => {
+    const oneCat = await axios.get(`${URL_PREFIX}/api/cats/${catId}`);
+
+    return oneCat;
+  },
 };
 
 export default API;
