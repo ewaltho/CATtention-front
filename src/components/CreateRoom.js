ee4321f77de18dd5dfe0f410baaa85b1bb4341e9
Ben L <veloswim@gmail.com>
2023-03-08 15:03:18 -0800

    added handle input change for room prefs

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -2,17 +2,53 @@ import React, { useState } from "react";
 import "../assets/css/CreateRoom.css";
 
 export default function CreateRoom(props) {
-  const [roomPreferences, setRoomPreferences] = useState({});
+  const [roomPreferences, setRoomPreferences] = useState({
+    minigameToggle: false,
+  });
   //   room prefs need the following keys: minigames bool, workTime, breakTime, room name needs to create a room with socket.
 
-  const handleRoomPrefsInputChange = (e) => {};
+  const handleRoomPrefsInputChange = (e) => {
+    // e.preventDefault();
+    if (
+      e.target.name === "minigameToggle" &&
+      roomPreferences.minigameToggle === false
+    ) {
+      setRoomPreferences({
+        ...roomPreferences,
+        minigameToggle: true,
+      });
+    } else if (
+      e.target.name === "minigameToggle" &&
+      roomPreferences.minigameToggle === true
+    ) {
+      setRoomPreferences({
+        ...roomPreferences,
+        minigameToggle: false,
+      });
+    } else {
+      setRoomPreferences({
+        ...roomPreferences,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
 
   return (
-    <div>
-      <label class="switch">
-        <input type="checkbox"></input>
-        <span class="slider"></span>
+    <form>
+      <label htmlFor="workTime">Work Timer:</label>
+      <input
+        name="workTime"
+        placeholder="Minutes to work"
+        onChange={handleRoomPrefsInputChange}
+      ></input>
+      <label className="switch">
+        <input
+          type="checkbox"
+          name="minigameToggle"
+          onChange={handleRoomPrefsInputChange}
+        ></input>
+        <span className="slider"></span>
       </label>
-    </div>
+    </form>
   );
 }
