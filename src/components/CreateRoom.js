15e61288bae7b7ab391fc0d4e0bc8f7e4caa222c
Ben L <veloswim@gmail.com>
2023-03-08 15:22:51 -0800

    added localstorage to form and handling more form elements with state

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/css/CreateRoom.css";
 
 export default function CreateRoom(props) {
@@ -6,6 +6,15 @@ export default function CreateRoom(props) {
     minigameToggle: false,
   });
   //   room prefs need the following keys: minigames bool, workTime, breakTime, room name needs to create a room with socket.
+
+  // ! On page load, if there are previous prefs, this will automatically fill them into our state.
+  useEffect(() => {
+    const savedRoomPrefObj = JSON.parse(localStorage.getItem("roomPrefs"));
+    console.log(savedRoomPrefObj);
+    if (savedRoomPrefObj) {
+      setRoomPreferences(savedRoomPrefObj);
+    }
+  }, []);
 
   const handleRoomPrefsInputChange = (e) => {
     // e.preventDefault();
@@ -33,13 +42,34 @@ export default function CreateRoom(props) {
     }
   };
 
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    localStorage.setItem("roomPrefs", JSON.stringify(roomPreferences));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmission}>
       <label htmlFor="workTime">Work Timer:</label>
       <input
         name="workTime"
         placeholder="Minutes to work"
         onChange={handleRoomPrefsInputChange}
+        value={roomPreferences.workTime}
+      ></input>
+      <label htmlFor="workTime">Break Timer:</label>
+      <input
+        name="breakTime"
+        placeholder="Minutes to break"
+        onChange={handleRoomPrefsInputChange}
+        value={roomPreferences.breakTime}
+      ></input>
+      {/* ! This needs to be made to create a room. I will speak with alex about how to do this later this evening. */}
+      <label htmlFor="workTime">Room Name:</label>
+      <input
+        name="roomName"
+        placeholder="Room Name"
+        onChange={handleRoomPrefsInputChange}
+        value={roomPreferences.roomName}
       ></input>
       <label className="switch">
         <input
@@ -49,6 +79,7 @@ export default function CreateRoom(props) {
         ></input>
         <span className="slider"></span>
       </label>
+      <button type="submit">Create Room</button>
     </form>
   );
 }
