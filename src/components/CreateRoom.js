1812d3f190c80af5ea66c36c9ddcb6657f02d86f
Ben L <veloswim@gmail.com>
2023-03-08 14:28:49 -0800

    added slider switch

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import "../assets/css/CreateRoom.css";
 
 export default function CreateRoom(props) {
   const [roomPreferences, setRoomPreferences] = useState({});
+  //   room prefs need the following keys: minigames bool, workTime, breakTime, room name needs to create a room with socket.
 
-  return <div>CreateRoom</div>;
+  const handleRoomPrefsInputChange = (e) => {};
+
+  return (
+    <div>
+      <label class="switch">
+        <input type="checkbox"></input>
+        <span class="slider"></span>
+      </label>
+    </div>
+  );
 }
