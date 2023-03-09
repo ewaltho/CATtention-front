ca2d4489d743e3eff1a1ec958a520e58be3d08b0
Ben L <veloswim@gmail.com>
2023-03-09 12:48:14 -0800

    added comments, timer now counts down and switches between break/work

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 
 export default function Timer({ roomPreferences }) {
   const [timerText, setTimerText] = useState("");
+  // ! started will be used later on
   const [started, setStarted] = useState(false);
   const [workState, setWorkState] = useState(false);
+  const [breakState, setBreakState] = useState(false);
+
+  // Grab our work and break seconds from roomprefs on previous page
+  // todo: There is a bug with a user entering something that will not be "cleanly" divisible. We need to control these inputs on the previous page.
   let workTimeSeconds = roomPreferences.workTime * 60;
   let breakTimeSeconds = roomPreferences.breakTime * 60;
 
+  // Starts work timer, this will count down on the page.
   const startWorkTimer = () => {
     setWorkState(true);
+    setBreakState(false);
 
     const countDown = () => {
+      console.log("workint");
       let minutes = Math.floor(workTimeSeconds / 60);
       let seconds = workTimeSeconds % 60;
       switch (seconds) {
@@ -61,18 +69,23 @@ export default function Timer({ roomPreferences }) {
       workTimeSeconds -= 1;
       if (workTimeSeconds <= 0) {
         clearInterval(workInterval);
-        setWorkState(false);
+        setTimerText(`Time for a break!`);
+
         setStarted(true);
         breakTimer();
+      } else {
+        setTimerText(`${minutes}:${seconds}`);
       }
-      setTimerText(`${minutes}:${seconds}`);
     };
 
     const workInterval = setInterval(countDown, 1000);
   };
 
+  // Break timer, same as above, just using breakTimeSeconds instead.
   const breakTimer = () => {
+    setBreakState(true);
     const countDown = () => {
+      console.log("breakint");
       let minutes = Math.floor(breakTimeSeconds / 60);
       let seconds = breakTimeSeconds % 60;
       switch (seconds) {
@@ -123,22 +136,23 @@ export default function Timer({ roomPreferences }) {
       breakTimeSeconds -= 1;
       if (breakTimeSeconds <= 0) {
         clearInterval(breakInterval);
-        setWorkState(true);
+        setWorkState(false);
         setTimerText(`Time's Up!`);
+      } else {
+        setTimerText(`${minutes}:${seconds}`);
       }
-      setTimerText(`${minutes}:${seconds}`);
     };
     const breakInterval = setInterval(countDown, 1000);
   };
 
   return (
     <div>
-      {workState ? <h1>Work Time!</h1> : <h1>Break Time</h1>}
+      {!breakState ? <h1>Work Time!</h1> : <h1>Break Time</h1>}
       <h2>{timerText}</h2>
       {workState === false ? (
         <button onClick={startWorkTimer}>Get to work!</button>
       ) : (
-        <button>Wait!</button>
+        <></>
       )}
     </div>
   );
