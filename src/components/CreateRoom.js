e5bbcfa71dc165b9ee1eef3cc5d64ae9520edfb8
Ben L <veloswim@gmail.com>
2023-03-08 17:11:14 -0800

    added a clear preferences button

--- src/components/CreateRoom.js
+++ src/components/CreateRoom.js
@@ -42,44 +42,58 @@ export default function CreateRoom(props) {
     }
   };
 
+  const clearAllPreferences = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("roomPrefs");
+    setRoomPreferences({
+      minigameToggle: false,
+    });
+  };
+
   const handleFormSubmission = (e) => {
     e.preventDefault();
     localStorage.setItem("roomPrefs", JSON.stringify(roomPreferences));
   };
 
   return (
-    <form onSubmit={handleFormSubmission}>
-      <label htmlFor="workTime">Work Timer:</label>
-      <input
-        name="workTime"
-        placeholder="Minutes to work"
-        onChange={handleRoomPrefsInputChange}
-        value={roomPreferences.workTime}
-      ></input>
-      <label htmlFor="workTime">Break Timer:</label>
-      <input
-        name="breakTime"
-        placeholder="Minutes to break"
-        onChange={handleRoomPrefsInputChange}
-        value={roomPreferences.breakTime}
-      ></input>
-      {/* ! This needs to be made to create a room. I will speak with alex about how to do this later this evening. */}
-      <label htmlFor="workTime">Room Name:</label>
-      <input
-        name="roomName"
-        placeholder="Room Name"
-        onChange={handleRoomPrefsInputChange}
-        value={roomPreferences.roomName}
-      ></input>
-      <label className="switch">
+    <>
+      <form onSubmit={handleFormSubmission}>
+        <label htmlFor="workTime">Work Timer:</label>
         <input
-          type="checkbox"
-          name="minigameToggle"
+          name="workTime"
+          placeholder="Minutes to work"
           onChange={handleRoomPrefsInputChange}
+          value={roomPreferences.workTime}
         ></input>
-        <span className="slider"></span>
-      </label>
-      <button type="submit">Create Room</button>
-    </form>
+        <label htmlFor="workTime">Break Timer:</label>
+        <input
+          name="breakTime"
+          placeholder="Minutes to break"
+          onChange={handleRoomPrefsInputChange}
+          value={roomPreferences.breakTime}
+        ></input>
+        {/* ! This needs to be made to create a room. I will speak with alex about how to do this later this evening. */}
+        <label htmlFor="workTime">Room Name:</label>
+        <input
+          name="roomName"
+          placeholder="Room Name"
+          onChange={handleRoomPrefsInputChange}
+          value={roomPreferences.roomName}
+        ></input>
+        <label className="switch">
+          <input
+            type="checkbox"
+            name="minigameToggle"
+            onChange={handleRoomPrefsInputChange}
+          ></input>
+          <span className="slider"></span>
+        </label>
+        <button type="submit">Create Room</button>
+      </form>
+
+      <button type="button" onClick={clearAllPreferences}>
+        Clear Preferences
+      </button>
+    </>
   );
 }
