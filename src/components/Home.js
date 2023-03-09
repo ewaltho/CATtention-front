63c9c884b02f0fe2920608cf694e3059dc3c2fd2
Ben L <veloswim@gmail.com>
2023-03-08 21:21:28 -0800

    removed create room from joinchat.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,16 +9,30 @@ function HomePage() {
         with a friend. You can level up with mini-games and earn cat badges to
         customize your profile! Now go! Be productive!
       </p>
-        <Link to="/signup" className="register-btn">
-          Register
+      <Link to="/signup" className="register-btn">
+        Register
+      </Link>
+      <Link to="/createroom" className="register-btn">
+        Create A Room
+      </Link>
+      <Link to="/joinchat" className="register-btn">
+        Join A Room
+      </Link>
+      <div className="bottom-box">
+        <img
+          src="wave-cat-black.png"
+          alt="drawing of cat waving"
+          className="cat-left cat"
+        />
+        <Link to="/" className="tipsBtn">
+          From the community
         </Link>
-        <div className="bottom-box">
-          <img src="wave-cat-black.png" alt="drawing of cat waving" className="cat-left cat"/>
-          <Link to="/" className="tipsBtn">
-            From the community
-          </Link>
-          <img src="stretch-cat-black.png" alt="drawing of cat stretching" className="cat-right cat"/>
-        </div>
+        <img
+          src="stretch-cat-black.png"
+          alt="drawing of cat stretching"
+          className="cat-right cat"
+        />
+      </div>
     </div>
   );
 }
