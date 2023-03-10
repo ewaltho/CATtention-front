6842d59c49f8005181584f7781a9ada15243dc71
Ben L <veloswim@gmail.com>
2023-03-10 10:36:41 -0800

    added functionality to trivia questions

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -5,24 +5,49 @@ export default function Trivia({ userObject }) {
   const [answersArr, setAnswersArr] = useState([]);
   const [correctAns, setCorrectAns] = useState("");
   const [question, setQuestion] = useState("");
+  const [questionID, setQuestionID] = useState("");
+  const [questionAnswered, setQuestionAnswered] = useState(false);
+  let userScore;
 
   useEffect(() => {
     API.randomTriviaQuestion().then((res) => {
       let answers = res.data.answers;
+      setQuestionID(res.data.id);
       setQuestion(res.data.question);
       setCorrectAns(res.data.correct_answer);
       setAnswersArr(answers.split(";"));
     });
   }, []);
+
+  const checkAnswer = async (e) => {
+    e.preventDefault();
+    const returnedQuestion = await API.getTriviaByID(questionID);
+    if (returnedQuestion.data.correct_answer === e.target.textContent) {
+      const score = await API.addScoreToUser(userObject.id, 1);
+      userScore = score.data.newScore;
+      console.log(score);
+    }
+    setQuestionAnswered(true);
+  };
+
   return (
     <div>
       <h2>A wild trivia question spawned!</h2>
       <h3>{question}</h3>
-      <ul>
-        {answersArr.map((ans, index) => (
-          <button key={index}>{ans}</button>
-        ))}
-      </ul>
+      {!questionAnswered ? (
+        <ul>
+          {answersArr.map((ans, index) => (
+            <button key={index} onClick={checkAnswer}>
+              {ans}
+            </button>
+          ))}
+        </ul>
+      ) : (
+        <p>
+          The correct answer is {correctAns}, and your score is now
+          {userScore}!{" "}
+        </p>
+      )}
     </div>
   );
 }
