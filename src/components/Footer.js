09597ded40b7d62d82059dc6bfc271d02c6fa707
VisualViolet <savannah_b_miller@outlook.com>
2023-03-13 14:38:20 -0700

    added footer element with copyright and link to github

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer () {
+    return (
+        <div className='footer'>
+            <a href='https://github.com/ewaltho/CATtention-front' target="_blank">&copy; CATtention 2023</a>
+        </div>
+    );
+}
