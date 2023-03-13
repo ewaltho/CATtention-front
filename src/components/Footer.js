21675303b3f8ea69f43a97eacb085e46b1bf5504
VisualViolet <savannah_b_miller@outlook.com>
2023-03-13 15:00:35 -0700

    added icon for github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Footer () {
     return (
         <div className='footer'>
-            <a href='https://github.com/ewaltho/CATtention-front' target="_blank">&copy; CATtention 2023</a>
+            <div className='footer-content'>
+                <b>&copy; CATtention 2023 </b>
+                <a href='https://github.com/ewaltho/CATtention-front' target="_blank"><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" /></a>
+            </div>
         </div>
     );
 }
