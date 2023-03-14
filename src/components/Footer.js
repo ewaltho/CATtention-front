a8de5018c98e75ed1cea4d7ff707a025cb301657
Alex <alexhallinthecloud@icloud.com>
2023-03-14 11:10:39 -0700

    added share buttons in footer to share website link directly to facebook and twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon} from 'react-share';
+
 
 export default function Footer () {
+    const shareUrl = 'https://cattention.netlify.app/';
+const quote = 'Check out this awesome website that helps you focus on work!';
     return (
         <div className='footer'>
             <div className='footer-content'>
                 <b>&copy; CATtention 2023 </b>
                 <a href='https://github.com/ewaltho/CATtention-front' target="_blank"><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" /></a>
+                <FacebookShareButton url={shareUrl} quote={quote}>
+                <FacebookIcon size={32} round={true} />
+                 Share on Facebook
+                </FacebookShareButton>
+                <TwitterShareButton url={shareUrl} quote={quote}>
+                <TwitterIcon size={32} round={true} />
+                Share on Twitter
+                </TwitterShareButton>
             </div>
         </div>
     );
